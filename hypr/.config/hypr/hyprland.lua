28dd9046d1a581cd5c25af6e8954d641b1c3b938
sigdown <vasilybichutsky@gmail.com>
2026-08-06 18:44:26 +0300

    feat(mount): added auto external dev mount

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -57,6 +57,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
 
     hl.exec_cmd("hypridle")
+    hl.exec_cmd("udiskie --smart-tray")
 end)
 
 -------------------------------
