d984272301761e420c32d3ff100ba18eed65d811
sigdown <vasilybichutsky@gmail.com>
2026-08-06 17:12:34 +0300

    feat(mako): add notification daemon

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -51,6 +51,7 @@ local menu        = "fuzzel"
 hl.on("hyprland.start", function()
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("waybar")
+    hl.exec_cmd("mako")
 end)
 
 -------------------------------
