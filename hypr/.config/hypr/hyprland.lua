f5ab7001d150d16eb3f9bea9d9f492600f3e94a9
sigdown <vasilybichutsky@gmail.com>
2026-08-06 12:00:13 +0300

    feat(hypr): add us and ru layouts

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -223,10 +223,10 @@ hl.config({
 
 hl.config({
     input = {
-        kb_layout  = "us",
+        kb_layout  = "us,ru",
         kb_variant = "",
         kb_model   = "",
-        kb_options = "",
+        kb_options = "grp:alt_shift_toggle",
         kb_rules   = "",
 
         follow_mouse = 1,
