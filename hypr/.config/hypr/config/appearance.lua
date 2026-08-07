a3154879b2377bdd374418192b5ee2f89c3c44cd
sigdown <vasilybichutsky@gmail.com>
2026-08-07 17:04:54 +0000

    style(surf): temporarily removed transparency because i like it better ths way

--- hypr/.config/hypr/config/appearance.lua
+++ hypr/.config/hypr/config/appearance.lua
@@ -33,7 +33,7 @@ hl.config({
         },
 
         blur = {
-            enabled  = true,
+            enabled  = false,
             size     = 3,
             passes   = 2,
             vibrancy = 0.16,
