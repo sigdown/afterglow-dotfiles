611feeecd88e214a47b58decc2cff56887ed4f17
sigdown <vasilybichutsky@gmail.com>
2026-08-08 13:50:42 +0000

    chore: update base package list; formated lua configs; remove deadcode

--- hypr/.config/hypr/config/startup.lua
+++ hypr/.config/hypr/config/startup.lua
@@ -17,10 +17,6 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("waybar")
     hl.exec_cmd("mako")
-
-    hl.exec_cmd("wl-paste --type text --watch cliphist store")
-    hl.exec_cmd("wl-paste --type image --watch cliphist store")
-
     hl.exec_cmd("hypridle")
     hl.exec_cmd("udiskie --smart-tray")
 end)
